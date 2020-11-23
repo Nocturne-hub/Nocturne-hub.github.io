8841d10e12c2588d3b4d99b2b0e9696ac16e8780
Nocturne-Hub <jeremy.battaglia0@gmail.com>
2020-11-23 11:47:19 +0100

    manifest

--- sw.js
+++ sw.js
@@ -19,17 +19,16 @@ self.addEventListener('install', (e) => {
       );
 });
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-      caches.match(e.request).then((r) => {
-            console.log('[Service Worker] Fetching resource: '+e.request.url);
-        return r || fetch(e.request).then((response) => {
-                  return caches.open(cacheName).then((cache) => {
-            console.log('[Service Worker] Caching new resource: '+e.request.url);
-            cache.put(e.request, response.clone());
-            return response;
-          });
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.open('mysite-dynamic').then(function (cache) {
+      return cache.match(event.request).then(function (response) {
+        var fetchPromise = fetch(event.request).then(function (networkResponse) {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
         });
-      })
-    );
-  });
+        return response || fetchPromise;
+      });
+    }),
+  );
+});
